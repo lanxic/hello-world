01f9e128574ba351b71f06a9f152461272e72285
Alex Manroe <lanxic@gmail.com>
2024-02-22 11:52:08 +0800

    update response

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return 'Hello World from CMI Today in {}'.format(current_date)
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return 'Hello World from CMI X Today in {}'.format(current_date)
 
 # main driver function
 if __name__ == '__main__':
-
-	app.run(debug=True, host="0.0.0.0", port=80)
+    app.run(debug=True, host="0.0.0.0", port=80)
