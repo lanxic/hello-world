4713309ff5c04770d04b4cbba130856ede381575
Alex Manroe <lanxic@gmail.com>
2024-02-22 11:42:05 +0800

    update response

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 	current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f'Hello World from CMI X series in {current_date}'
+    return f'Hello World from CMI X Today in {current_date}'
 
 # main driver function
 if __name__ == '__main__':
