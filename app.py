70b1280058f229588f76852095bbe6791b98cb48
Alex Manroe <lanxic@gmail.com>
2024-02-22 11:24:48 +0800

    update response

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World from AWS-EKS test X'
+	current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f'Hello World from CMI X series in {current_date}'
 
 # main driver function
 if __name__ == '__main__':
