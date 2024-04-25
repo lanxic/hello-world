9432dafff35fbaa776aa619618363cb26f7105d5
Alex Manroe <lanxic@gmail.com>
2024-04-25 17:15:27 +0800

    update jenkins

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return 'Hello World from CMI X Today in {}'.format(current_date)
+    return 'Hello World from CMI X Today in {} is rain'.format(current_date)
 
 # main driver function
 if __name__ == '__main__':
